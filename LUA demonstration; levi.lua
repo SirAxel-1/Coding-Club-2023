90f165daf658e93dbffe112dd158eebb57160237
Sponono1 <143684310+Sponono1@users.noreply.github.com>
2023-08-31 13:41:37 +0800

    Create LUA demonstration; levi.lua

---
+++ LUA demonstration; levi.lua
@@ -0,0 +1,12 @@
+local hasRobux = true
+
+if hasRobux == true then
+  print('You can afford this item.')
+else
+  print("You can't afford this item.")
+end
+
+
+
+
+--this script checks if you can afford the item with the hasRobux variable.
