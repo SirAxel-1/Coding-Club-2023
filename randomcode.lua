8c4b7f5c48eff81750d157e1f5d919f24144e3c3
Sponono1 <143684310+Sponono1@users.noreply.github.com>
2023-09-11 13:09:35 +0800

    Create randomcode.lua

---
+++ randomcode.lua
@@ -0,0 +1,24 @@
+local robuxNumber = 35
+
+local function buyItem(robuxNumber, robuxNumber)
+ if robuxNumber >= 35 then
+    print("You have bought the item, GG!")
+ elseif
+   robuxNumber < 35 then
+     print("Too poor.")
+ end
+end
+
+buyItem(35, 35)
+
+
+local doMath = true
+
+local function loadMath(Number1, Number2)
+    if doMath == true then
+        print(Number1 * Number2)
+    end
+
+
+    loadMath()
+--this is just some code i made, does some things! put some numbers in loadMath() for example; loadMatch(100, 200) (dont forget the space!) will times 100 by 200! 
